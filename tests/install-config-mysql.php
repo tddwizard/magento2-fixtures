15d20fcbeb0579409cc6198b2479c6eb7632079b
Fabian Schmengler <fs@integer-net.de>
2017-08-05 15:55:23 +0200

    Add wercker configuration for CI

---
+++ tests/install-config-mysql.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+return [
+    'db-host' => 'DB_HOST',
+    'db-user' => 'DB_USER',
+    'db-password' => 'DB_PASSWORD',
+    'db-name' => 'DB_NAME',
+    'db-prefix' => '',
+    'backend-frontname' => 'backend',
+    'admin-user' => \Magento\TestFramework\Bootstrap::ADMIN_NAME,
+    'admin-password' => \Magento\TestFramework\Bootstrap::ADMIN_PASSWORD,
+    'admin-email' => \Magento\TestFramework\Bootstrap::ADMIN_EMAIL,
+    'admin-firstname' => \Magento\TestFramework\Bootstrap::ADMIN_FIRSTNAME,
+    'admin-lastname' => \Magento\TestFramework\Bootstrap::ADMIN_LASTNAME,
+];
