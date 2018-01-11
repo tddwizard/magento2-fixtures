7e6ecdb311c50b6b99a958c44b723a3b7c3b536a
Fabian Schmengler <fs@integer-net.de>
2018-01-11 14:13:54 +0100

    Fix compilation error in Magento 2.1

--- src/phpunit4.php
+++ src/phpunit4.php
@@ -2,7 +2,11 @@
 
 namespace PHPUnit\Framework {
 
-    if (!class_exists(TestCase::class)) {
+    /*
+     * TestCase::class cannot be used because it would break code generation in Magento 2.1
+     * where the code scanner looks for the first "class" keyword.
+     */
+    if (!class_exists('\PHPUnit\Framework\TestCase')) {
         /*
          * Forward compatibility for PHPUnit 4.x (Magento 2.1)
          */
